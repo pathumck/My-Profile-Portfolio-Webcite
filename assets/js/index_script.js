cd9d14170ed10f8343270e530a9705ec7910d4e8
pathum <ckpathumck@gmail.com>
2024-05-13 09:15:48 +0530

    create <a> s click event color

---
+++ assets/js/index_script.js
@@ -0,0 +1,39 @@
+document.querySelector("nav>ul>li>a[href='#start']").addEventListener("click",function(event){
+    this.style.color = "#ff6348";
+    document.querySelector("nav>ul>li>a[href='#aboutme']").style.color = "white"
+    document.querySelector("nav>ul>li>a[href='#gallery']").style.color = "white"
+    document.querySelector("nav>ul>li>a[href='#proceedings']").style.color = "white"
+    document.querySelector("nav>ul>li>a[href='#contactme']").style.color = "white"
+});
+
+document.querySelector("nav>ul>li>a[href='#aboutme']").addEventListener("click",function(event){
+    this.style.color = "#ff6348";
+    document.querySelector("nav>ul>li>a[href='#start']").style.color = "white"
+    document.querySelector("nav>ul>li>a[href='#gallery']").style.color = "white"
+    document.querySelector("nav>ul>li>a[href='#proceedings']").style.color = "white"
+    document.querySelector("nav>ul>li>a[href='#contactme']").style.color = "white"
+});
+
+document.querySelector("nav>ul>li>a[href='#proceedings']").addEventListener("click",function(event){
+    this.style.color = "#ff6348";
+    document.querySelector("nav>ul>li>a[href='#start']").style.color = "white"
+    document.querySelector("nav>ul>li>a[href='#gallery']").style.color = "white"
+    document.querySelector("nav>ul>li>a[href='#aboutme']").style.color = "white"
+    document.querySelector("nav>ul>li>a[href='#contactme']").style.color = "white"
+});
+
+document.querySelector("nav>ul>li>a[href='#gallery']").addEventListener("click",function(event){
+    this.style.color = "#ff6348";
+    document.querySelector("nav>ul>li>a[href='#start']").style.color = "white"
+    document.querySelector("nav>ul>li>a[href='#proceedings']").style.color = "white"
+    document.querySelector("nav>ul>li>a[href='#aboutme']").style.color = "white"
+    document.querySelector("nav>ul>li>a[href='#contactme']").style.color = "white"
+});
+
+document.querySelector("nav>ul>li>a[href='#contactme']").addEventListener("click",function(event){
+    this.style.color = "#ff6348";
+    document.querySelector("nav>ul>li>a[href='#start']").style.color = "white"
+    document.querySelector("nav>ul>li>a[href='#proceedings']").style.color = "white"
+    document.querySelector("nav>ul>li>a[href='#aboutme']").style.color = "white"
+    document.querySelector("nav>ul>li>a[href='#gallery']").style.color = "white"
+});
