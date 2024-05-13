4372650da0d7bf81d04d50d7dae54c80f96b5451
pathum <ckpathumck@gmail.com>
2024-05-13 13:37:42 +0530

    update <a> s hover effect

--- assets/js/index_script.js
+++ assets/js/index_script.js
@@ -1,39 +1,35 @@
 document.querySelector("nav>ul>li>a[href='#start']").addEventListener("click",function(event){
-    this.style.color = "#ff6348";
-    document.querySelector("nav>ul>li>a[href='#aboutme']").style.color = "white"
-    document.querySelector("nav>ul>li>a[href='#gallery']").style.color = "white"
-    document.querySelector("nav>ul>li>a[href='#proceedings']").style.color = "white"
-    document.querySelector("nav>ul>li>a[href='#contactme']").style.color = "white"
+    document.querySelectorAll("nav>ul>li>a").forEach(function(link) {
+        link.classList.remove("active");
+    });
+
+    this.classList.add("active");
 });
 
 document.querySelector("nav>ul>li>a[href='#aboutme']").addEventListener("click",function(event){
-    this.style.color = "#ff6348";
-    document.querySelector("nav>ul>li>a[href='#start']").style.color = "white"
-    document.querySelector("nav>ul>li>a[href='#gallery']").style.color = "white"
-    document.querySelector("nav>ul>li>a[href='#proceedings']").style.color = "white"
-    document.querySelector("nav>ul>li>a[href='#contactme']").style.color = "white"
+    document.querySelectorAll("nav>ul>li>a").forEach(function(link) {
+        link.classList.remove("active");
+    });
+    this.classList.add("active");
 });
 
 document.querySelector("nav>ul>li>a[href='#proceedings']").addEventListener("click",function(event){
-    this.style.color = "#ff6348";
-    document.querySelector("nav>ul>li>a[href='#start']").style.color = "white"
-    document.querySelector("nav>ul>li>a[href='#gallery']").style.color = "white"
-    document.querySelector("nav>ul>li>a[href='#aboutme']").style.color = "white"
-    document.querySelector("nav>ul>li>a[href='#contactme']").style.color = "white"
+    document.querySelectorAll("nav>ul>li>a").forEach(function(link) {
+        link.classList.remove("active");
+    });
+    this.classList.add("active");
 });
 
 document.querySelector("nav>ul>li>a[href='#gallery']").addEventListener("click",function(event){
-    this.style.color = "#ff6348";
-    document.querySelector("nav>ul>li>a[href='#start']").style.color = "white"
-    document.querySelector("nav>ul>li>a[href='#proceedings']").style.color = "white"
-    document.querySelector("nav>ul>li>a[href='#aboutme']").style.color = "white"
-    document.querySelector("nav>ul>li>a[href='#contactme']").style.color = "white"
+    document.querySelectorAll("nav>ul>li>a").forEach(function(link) {
+        link.classList.remove("active");
+    });
+    this.classList.add("active");
 });
 
 document.querySelector("nav>ul>li>a[href='#contactme']").addEventListener("click",function(event){
-    this.style.color = "#ff6348";
-    document.querySelector("nav>ul>li>a[href='#start']").style.color = "white"
-    document.querySelector("nav>ul>li>a[href='#proceedings']").style.color = "white"
-    document.querySelector("nav>ul>li>a[href='#aboutme']").style.color = "white"
-    document.querySelector("nav>ul>li>a[href='#gallery']").style.color = "white"
+    document.querySelectorAll("nav>ul>li>a").forEach(function(link) {
+        link.classList.remove("active");
+    });
+    this.classList.add("active");
 });
